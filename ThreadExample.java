976c97c8de0136ca823b8dc83353982aef9b0aba
Santosh-Kumar-Sah <sureshsirf886@gmail.com>
2025-09-26 14:05:02 +0530

    25-09-2025

--- ThreadExample.java
+++ ThreadExample.java
@@ -2,6 +2,11 @@ class ThreadDemo extends Thread{
     public void run(){
         for(int i=0;i<10;i++){
             System.out.println(Thread.currentThread().getName()+"is running");
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+                System.out.println(e);
+            }
         }
     }
 }
@@ -9,8 +14,11 @@ class ThreadDemo extends Thread{
 public class ThreadExample {
     public static void main(String[] args) {
         ThreadDemo t1 = new ThreadDemo();//thread 0
+        // set name
+        t1.setName("first thread");
         t1.start();//no need to call run (start will auto do it)
         ThreadDemo t2 = new ThreadDemo();//thread 1
+        t2.setName("second thread");
         t2.start();//no need to call run (start will auto do it)
     }
     
